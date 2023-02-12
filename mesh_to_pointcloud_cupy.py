c91db877c91c0c914b1f474c86841e72ed77fb85
Charbel199 <charbel-boumaroun@outlook.com>
2023-02-12 12:21:25 +0200

    Move cupy code to separate file

--- mesh_to_pointcloud_cupy.py
+++ mesh_to_pointcloud_cupy.py
@@ -1,27 +1,4 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
-
-# Copyright (c) 2023 Jakub Uhlik
-
-# This code is meant to use Jakob Uhlik's blender triangle surface sampler which can be found at https://github.com/uhlik/bpy/blob/master/space_view3d_point_cloud_visualizer.py,
-# optimize it and add new functionalities to it with the aim to use it purely in code (Not as a blender add-on).
-
-import numpy as np
+# import numpy as np
 import cupy as np
 import bmesh
 import logging
