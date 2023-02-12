6431ac08bb4cbf53d8635d75968f12b27a2915e6
Charbel199 <charbel-boumaroun@outlook.com>
2023-02-12 12:57:47 +0200

    Vectorize area and vertices code

--- mesh_to_pointcloud_numpy.py
+++ mesh_to_pointcloud_numpy.py
@@ -8,6 +8,7 @@ from mathutils.interpolate import poly_3d_calc
 import math
 import logging
 import time
+from itertools import repeat
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,11 +21,12 @@ class TriangleSurfaceSampler:
     def mesh_to_pointcloud(context, o, num_samples, rnd, colorize=None, constant_color=None,
                            compute_normals=False, exact_number_of_points=False):
 
-        vs_time = []
-        pre_vs_time = []
         num_samples = int(num_samples)
+
+        # ================================================
+        # Prepare Mesh
+        # ================================================
         start = time.time_ns() / (10 ** 9)
-        # Mesh stuff
         depsgraph = context.evaluated_depsgraph_get()
         if (o.modifiers):
             owner = o.evaluated_get(depsgraph)
@@ -40,39 +42,54 @@ class TriangleSurfaceSampler:
         if (len(bm.faces) == 0):
             raise Exception("Mesh has no faces")
         stop = time.time_ns() / (10 ** 9)
-        logging.info(f"Blender-Preprocessing took {stop-start}")
+        logging.info(f"Blender-Preprocessing took {stop - start}")
+
+        polys = np.asarray(bm.faces)
+
+        # ================================================
+        # Prepare vertices
+        # ================================================
+        def _get_vertex(poly, index=0):
+            return poly.verts[index].co
+
+        v1_xyz = np.array(list(map(_get_vertex, polys, repeat(0))))
+        v2_xyz = np.array(list(map(_get_vertex, polys, repeat(1))))
+        v3_xyz = np.array(list(map(_get_vertex, polys, repeat(2))))
+
+        # ================================================
+        # Prepare areas
+        # ================================================
+        def _get_area(poly):
+            return poly.calc_area()
+
+        areas = np.array(list(map(_get_area, polys)))
 
 
-        # Areas and weighted indices
-        areas = [p.calc_area() for p in bm.faces]
-        areas = np.asarray(areas)
-        probabilities = areas/sum(areas)
+        # ================================================
+        # Prepare weighted random indices
+        # ================================================
+        probabilities = areas / sum(areas)
         weighted_random_indices = np.random.choice(range(len(areas)), size=num_samples, p=probabilities)
-        v1_xyz = []
-        v2_xyz = []
-        v3_xyz = []
-        for poly in bm.faces:
-            v1_xyz.append(poly.verts[0].co)
-            v2_xyz.append(poly.verts[1].co)
-            v3_xyz.append(poly.verts[2].co)
 
-        v1_xyz = np.asarray(v1_xyz)
-        v2_xyz = np.asarray(v2_xyz)
-        v3_xyz = np.asarray(v3_xyz)
-
+        # ================================================
+        # Fetch random polygons' vertices
+        # ================================================
         v1_xyz = v1_xyz[weighted_random_indices]
         v2_xyz = v2_xyz[weighted_random_indices]
         v3_xyz = v3_xyz[weighted_random_indices]
 
-        u = np.random.rand(num_samples,1)
-        v = np.random.rand(num_samples,1)
-        is_a_problem = u+v>1
+        # ================================================
+        # Create points
+        # ================================================
+        u = np.random.rand(num_samples, 1)
+        v = np.random.rand(num_samples, 1)
+        is_a_problem = u + v > 1
         u[is_a_problem] = 1 - u[is_a_problem]
         v[is_a_problem] = 1 - v[is_a_problem]
-        w = 1 - (u+v)
-
-        vs = (v1_xyz * u) + (v2_xyz * v) + (w*v3_xyz)
+        w = 1 - (u + v)
+        vs = (v1_xyz * u) + (v2_xyz * v) + (w * v3_xyz)
         vs = vs.astype(np.float)
+
         ns = []
         cs = np.full_like(vs, (1.0, 0.0, 0.0,))
         ns = np.array(ns, dtype=np.float)
